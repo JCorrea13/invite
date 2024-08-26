dec56ac1ce9329bcd90f61b74a4515ce09620d05
Manuel Correa <josemanuelcorrea.13@gmail.com>
2024-08-25 22:58:02 -0600

    fix final text

--- time.js
+++ time.js
@@ -3,19 +3,19 @@ function updateCountdown() {
     const now = new Date();
     const timeDifference = targetDate - now;
 
-    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+    const days = timeDifference < 0 ? 0 : Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = timeDifference < 0 ? 0 : Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = timeDifference < 0 ? 0 : Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = timeDifference < 0 ? 0 : Math.floor((timeDifference % (1000 * 60)) / 1000);
 
     document.getElementById('days').textContent = (days > 9 ? days : '0' + days) + ':';
     document.getElementById('hours').textContent = (hours > 9 ? hours : '0' + hours) + ':';
     document.getElementById('minutes').textContent = (minutes > 9 ? minutes : '0' + minutes) + ':';
     document.getElementById('seconds').textContent = (seconds > 9 ? seconds : '0' + seconds);
 
-    if (timeDifference < 0) {
+    if (days == 0) {
         clearInterval(interval);
-        document.getElementById('countdown').textContent = "ES HOY, ES HOY!";
+        document.getElementById('countdown').textContent = "¡ÉS HOY, ÉS HOY!";
     }
 }
 
